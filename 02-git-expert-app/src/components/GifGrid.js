204be868600fce5216c9e59085d9d6954cabcc6c
obbijuan <juan.garces@outlook.com>
2020-11-19 10:18:02 -0300

    Custom Hook Added

--- 02-git-expert-app/src/components/GifGrid.js
+++ 02-git-expert-app/src/components/GifGrid.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs(category)
-            .then( imgs => setImages(imgs))
-    }, [category])
+    // En la desectructuracion, se renombra data por images
+    const {data:images, loading} = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
+
+            {loading && <p>Loading...</p>}
+
             <div className="card-grid">
                 {
                     images.map(img => (
