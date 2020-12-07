79fd7fea412939adacf906c723c41e43adf46c6f
obbijuan <juan.garces@outlook.com>
2020-12-07 14:12:48 -0300

    Form with Custom Hook

---
+++ 03-hook-app/src/hooks/useForm.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+// Este custom hook se encarga de manejar los formularios
+export const useForm = (initialState = {}) => {
+    
+    const [values, setValues] = useState(initialState);
+    const handleInputChange = ({ target }) => {
+        setValues({
+            ...values,
+            [target.name] : target.value
+        })
+    }
+    // En el primer valor del arregle devuelve el estado del formulario
+    // y el segundo valor el handleInputChange.
+    return [values, handleInputChange]
+}
