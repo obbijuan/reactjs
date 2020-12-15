47cbf7ee3351f4a798f704b7d401588d4730ef96
obbijuan <juan.garces@outlook.com>
2020-12-15 01:06:59 -0300

    Real example useRef()

--- 03-hook-app/src/components/03-examples/MultipleCustomHooks.js
+++ 03-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useCounter } from '../../hooks/useCounter'
 import '../02-useEffect/effects.css'
 
@@ -6,8 +6,16 @@ export const MultipleCustomHooks = () => {
 
     const listMovies = ['tt0133093', 'tt0234215', 'tt0242653', 'tt0120737', 'tt0167261',
                         'tt0167260', 'tt0120915', 'tt0121765', 'tt0121766', 'tt8111088'];
+    const isMounted = useRef(true);
     const [datos, setDatos] = useState({});
     const { idMovie, next } = useCounter(listMovies);
+
+    useEffect(() => {
+
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
     
     
     useEffect(() => {
@@ -15,9 +23,11 @@ export const MultipleCustomHooks = () => {
         async function fetchData() {
             await fetch(`http://www.omdbapi.com/?i=${idMovie}&apikey=9064e370`)
             .then(resp => resp.json())
-            .then(data => {
-                const { Title, Year, Poster, Plot, Genre } = !!data && data
-                setDatos({loading:false,Title,Year,Poster,Plot,Genre})
+                .then(data => {
+                    if (isMounted.current) {
+                        const { Title, Year, Poster, Plot, Genre } = !!data && data
+                        setDatos({ loading: false, Title, Year, Poster, Plot, Genre })
+                    }
             })
         }
         fetchData();
