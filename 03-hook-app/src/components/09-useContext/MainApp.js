bd7fdfe1e72bdba7a265d06ca644ff06e5b09dfe
obbijuan <juan.garces@outlook.com>
2020-12-27 13:13:41 -0300

    CreateContext, useContext added

--- 03-hook-app/src/components/09-useContext/MainApp.js
+++ 03-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user = {
+        id: 123,
+        name: 'John',
+        email: 'john.g@gmail.com'
+    }
+
     return (
-        <AppRouter/>
+        <UserContext.Provider value={ user }>
+            <AppRouter/>
+        </UserContext.Provider>
     )
 }
