a1a3993451fc2069e6b5e0467003ae1ad0e39e3b
obbijuan <juan.garces@outlook.com>
2020-12-28 00:49:18 -0300

    Adding new tests on useCounter

--- 03-hook-app/src/tests/hooks/useCounter.test.js
+++ 03-hook-app/src/tests/hooks/useCounter.test.js
@@ -1,6 +1,5 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter';
-
 
 describe('Pruebas en useCounter', () => {
     test('debe retornar valores por defecto', () => {
@@ -10,6 +9,26 @@ describe('Pruebas en useCounter', () => {
         expect( typeof result.current.increment ).toBe('function');
         expect( typeof result.current.next ).toBe('function');
         
+    })
+
+    test('debe de tener el counter en 100', () => {
+        
+        const { result } = renderHook(() => useCounter([], 100));
+        // console.log(result.current)
+        expect( result.current.position ).toBe(100);
+        
+    })
+
+    test('debe incrementar el counter en 1', () => {
+        
+        const { result } = renderHook(() => useCounter([], 100));
+        const { increment } = result.current;
+        act(() => {
+            increment();
+        });
+        // console.log(result.current)
+        const { position } = result.current;
+        expect(position).toBe(101);
     })
     
 })
