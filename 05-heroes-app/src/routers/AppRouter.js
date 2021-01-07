32fb32b38e0f2468fc7af6dcb69b73ca6bba22be
obbijuan <juan.garces@outlook.com>
2021-01-06 23:25:25 -0300

    Creating second router

--- 05-heroes-app/src/routers/AppRouter.js
+++ 05-heroes-app/src/routers/AppRouter.js
@@ -5,18 +5,15 @@ import {
   Route
 } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
-import { Navbar } from "../components/ui/Navbar";
+import { DashboardRouters } from "./DashboardRouters";
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
-                <Navbar/>
-
                 <Switch>
                     <Route exact path="/login" component={ LoginScreen }/>
-                    <Route exact path="/" component={ MarvelScreen }/>
+                    <Route path="/" component={ DashboardRouters }/>
                 </Switch>
             </div>
         </Router>
