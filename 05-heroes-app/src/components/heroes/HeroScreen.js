cc5896bd7497ba925c16572033e1990970dcc0de
obbijuan <juan.garces@outlook.com>
2021-01-14 00:38:21 -0300

    Added animations

--- 05-heroes-app/src/components/heroes/HeroScreen.js
+++ 05-heroes-app/src/components/heroes/HeroScreen.js
@@ -34,10 +34,10 @@ export const HeroScreen = ({ history }) => {
                 <img
                     src={`../assets/heroes/${heroeId}.jpg`}
                     alt={superhero}
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__backInLeft"
                 />
             </div>
-            <div className="col-8">
+            <div className="col-8 animate__animated animate__fadeIn">
                 <h3>{superhero}</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
