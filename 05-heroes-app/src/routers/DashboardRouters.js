9bf5a3ece8f2050e9952b7a9e96e1f38f7315d72
obbijuan <juan.garces@outlook.com>
2021-01-14 09:22:22 -0300

    Added Search Component (SearchScreen)

--- 05-heroes-app/src/routers/DashboardRouters.js
+++ 05-heroes-app/src/routers/DashboardRouters.js
@@ -5,6 +5,7 @@ import { Redirect, Route, Switch } from 'react-router-dom'
 import { MarvelScreen } from '../components/marvel/MarvelScreen'
 import { HeroScreen } from '../components/heroes/HeroScreen'
 import { DcScreen } from '../components/dc/DcScreen'
+import { SearchScreen } from '../components/search/SearchScreen'
 
 
 export const DashboardRouters = () => {
@@ -16,7 +17,7 @@ export const DashboardRouters = () => {
                     <Route exact path="/marvel" component={MarvelScreen}/>
                     <Route exact path="/hero/:heroeId" component={HeroScreen}/>
                     <Route exact path="/dc" component={DcScreen} />
-
+                    <Route exact path="/search" component={SearchScreen} />
                     <Redirect to="/marvel" />
                 </Switch>
             </div>
