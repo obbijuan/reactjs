e6b687e19f8dad53c606aecfe210c3ed442b36c7
obbijuan <juan.garces@outlook.com>
2021-01-15 09:02:56 -0300

    Login implementation

--- 05-heroes-app/src/components/login/LoginScreen.js
+++ 05-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({ history }) => {
+
+    const { dispatch } = useContext(AuthContext)
+
     const handleLogin = () => {
-        history.push("/")
-        // history.replace("/")
+        // history.push("/")
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'obbijuan'
+            }
+        })
+        history.replace("/")
+        
     }
     return (
         <div className="container mt-5">
