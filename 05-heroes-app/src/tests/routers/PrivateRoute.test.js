e4a7904b5dfcd3f81f5c30190de25bea7ca45be4
obbijuan <juan.garces@outlook.com>
2021-01-17 12:11:27 -0300

    PrivateRoute tests

---
+++ 05-heroes-app/src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en <PrivateRoute/>', () => {
+
+    const props = {
+        location: {
+            pathname: '/marvel'
+        }
+    }
+    
+    test('debe de mostrar el componente si está autenticado y guardar localStorage', () => {
+
+        const wrapper = mount(
+            // (MemoryRouter): Es un HOC para hacer pruebas de Router con ciertas rutas para que se puedan evaluar
+            <MemoryRouter>
+                <PrivateRoute
+                    isAuthenticated={true}
+                    component={() => <span>Component Test!</span>}
+                    {...props}
+                />
+            </MemoryRouter>
+        )
+
+        expect(wrapper.find('span').exists()).toBe(true)
+    })
+    
+
+})
