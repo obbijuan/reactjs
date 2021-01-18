00ad913cb9618ecb501b9168d257ebd5be20e61b
obbijuan <juan.garces@outlook.com>
2021-01-18 00:15:55 -0300

    Tests on <Navbar/>

---
+++ 05-heroes-app/src/tests/components/ui/Navbar.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import '@testing-library/jest-dom';
+
+import { AuthContext } from '../../../auth/AuthContext';
+import { Navbar } from '../../../components/ui/Navbar';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { types } from '../../../types/types';
+
+describe('Prueas en <Navbar/>', () => {
+
+    const historyMock = {
+        push: jest.fn(),
+        replace: jest.fn(),
+        location: {},
+        listen: jest.fn(),
+        createHref: jest.fn(),
+    }
+
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            logged: true,
+            name: 'obbijuan'
+        }
+    }
+
+    const wrapper = mount(
+        <AuthContext.Provider value={contextValue}>
+            <MemoryRouter>
+                <Router history={historyMock}>
+                     <Navbar/>
+                </Router>
+            </MemoryRouter>
+        </AuthContext.Provider>
+    )
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
+    test('debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.text-info').text().trim()).toBe('obbijuan')
+    })
+
+    test('debe de llamar el logout y usar el history', () => {
+        
+        wrapper.find('button').prop('onClick')();
+
+        expect(contextValue.dispatch).toHaveBeenCalledWith({
+            type: types.logout
+        });
+
+        expect(historyMock.replace).toHaveBeenCalledWith('/login')
+
+    })
+    
+    
+})
