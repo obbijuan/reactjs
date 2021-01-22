9717ca8c9764707184bcfa2b5d428348a64ff4e1
obbijuan <juan.garces@outlook.com>
2021-01-21 23:45:33 -0300

    AuthRouter styles

--- 06-journal-app/src/routers/AuthRouter.js
+++ 06-journal-app/src/routers/AuthRouter.js
@@ -5,21 +5,22 @@ import { RegisterScreen } from '../components/auth/RegisterScreen'
 
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                <Route
-                    exact
-                    path="/auth/login"
-                    component={LoginScreen}
-                />
-                <Route
-                    exact
-                    path="/auth/register"
-                    component={RegisterScreen}
-                />
-                <Redirect to="/auth/login"/>
-
-            </Switch>
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route
+                        exact
+                        path="/auth/login"
+                        component={LoginScreen}
+                    />
+                    <Route
+                        exact
+                        path="/auth/register"
+                        component={RegisterScreen}
+                    />
+                    <Redirect to="/auth/login"/>
+                </Switch>
+            </div>
         </div>
     )
 }
