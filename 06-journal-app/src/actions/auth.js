87a7d814614c6b178eb3cd763522d8f90d348881
obbijuan <juan.garces@outlook.com>
2021-01-26 23:58:24 -0300

    Load states added to LoginScreen

--- 06-journal-app/src/actions/auth.js
+++ 06-journal-app/src/actions/auth.js
@@ -1,17 +1,22 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from '../types/types';
+import { finishLoading, startLoading } from './ui';
 
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
 
+        dispatch( startLoading() );
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(({ user }) => {
-                dispatch(login( user.uid, user.displayName))
+                dispatch(login(user.uid, user.displayName))
+                dispatch(finishLoading());
+                
             }).catch(e => {
                 console.log(e)
+                dispatch(finishLoading());
             })
-            
     }
 }
 
--- 06-journal-app/src/components/auth/LoginScreen.js
+++ 06-journal-app/src/components/auth/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm';
@@ -8,6 +8,7 @@ import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 export const LoginScreen = () => {
 
     const dispatch = useDispatch();
+    const { loading } = useSelector(state => state.ui)
 
     const [ formValues, handleInputChange ] = useForm({
         email: 'obbijuan@gmail.com',
@@ -55,6 +56,7 @@ export const LoginScreen = () => {
                 <button
                     type="submit"
                     className="btn btn-primary btn-block"
+                    disabled={ loading }
                 >
                     Login
                 </button>
