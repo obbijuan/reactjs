c30a2e8c76cd6c28334901dff9feba966c682c03
obbijuan <juan.garces@outlook.com>
2021-01-27 23:58:41 -0300

    Error messages: SweetAlert2 added

--- 06-journal-app/src/actions/auth.js
+++ 06-journal-app/src/actions/auth.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2'
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from '../types/types';
 import { finishLoading, startLoading } from './ui';
@@ -16,6 +17,7 @@ export const startLoginEmailPassword = (email, password) => {
             }).catch(e => {
                 console.log(e)
                 dispatch(finishLoading());
+                Swal.fire('Error', e.message, 'error');
             })
     }
 }
@@ -32,6 +34,7 @@ export const startRegisterWithEmailPasswordName = (email, password, name) => {
                 )
             }).catch(e => {
                 console.log(e)
+                Swal.fire('Error', e.message, 'error');
             })
     }
 }
