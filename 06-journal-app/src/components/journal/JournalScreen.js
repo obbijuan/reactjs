1e9f2df4fd01a636d44727d53c1d0ac9fabccbfb
obbijuan <juan.garces@outlook.com>
2021-01-28 23:51:22 -0300

    NoteReducer added

--- 06-journal-app/src/components/journal/JournalScreen.js
+++ 06-journal-app/src/components/journal/JournalScreen.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
-// import { NothingSelected } from './NothingSelected';
+import { NothingSelected } from './NothingSelected';
+import { useSelector } from 'react-redux';
 
 export const JournalScreen = () => {
+
+    // Se extrae las notas del state
+    const { active } = useSelector( state => state.notes );
+
     return (
         <div className="journal__main-content">
             
             <Sidebar />
 
             <main>
-                {/* <NothingSelected/> */}
-                <NoteScreen/>
+                {
+                    ( active ) 
+                        ? ( <NoteScreen /> )
+                        : ( <NothingSelected /> )
+                }
             </main>
 
         </div>
