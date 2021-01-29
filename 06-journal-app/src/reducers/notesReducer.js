cf09df2aedc9c7653b0ac5b21bd4b4481604b388
obbijuan <juan.garces@outlook.com>
2021-01-29 00:51:55 -0300

    Activate the created note

--- 06-journal-app/src/reducers/notesReducer.js
+++ 06-journal-app/src/reducers/notesReducer.js
@@ -1,3 +1,4 @@
+import { types } from "../types/types"
 
 
 const initialState = {
@@ -8,6 +9,14 @@ const initialState = {
 export const notesReducer = ( state = initialState, action ) => {
 
     switch (action.type) {
+
+        case types.notesActive:
+            return {
+                ...state,
+                active: {
+                    ...action.payload
+                }
+            }
 
         default:
             return state
