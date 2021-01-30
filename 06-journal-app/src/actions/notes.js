54ce525915710e55c15f3bc47856ca438c2ee75a
obbijuan <juan.garces@outlook.com>
2021-01-29 22:53:15 -0300

    Loading notes from Firestore

--- 06-journal-app/src/actions/notes.js
+++ 06-journal-app/src/actions/notes.js
@@ -27,3 +27,8 @@ export const activeNote = ( id, note ) => ({
         ...note
     }
 });
+
+export const setNotes = ( notes ) => ({
+    type: types.notesLoad,
+    payload: notes
+});
--- 06-journal-app/src/routers/AppRouter.js
+++ 06-journal-app/src/routers/AppRouter.js
@@ -14,6 +14,8 @@ import { PublicRoute } from './PublicRoute';
 
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { login } from '../actions/auth';
+import { loadNotes } from '../helpers/loadNotes';
+import { setNotes } from '../actions/notes';
 
 export const AppRouter = () => {
 
@@ -24,10 +26,15 @@ export const AppRouter = () => {
 
     useEffect(() => {
 
-        firebase.auth().onAuthStateChanged((user) => {
+        firebase.auth().onAuthStateChanged(async(user) => {
             if (user?.uid) {
+
                 dispatch(login(user.uid, user.displayName));
                 setIsLoggedIn(true);
+
+                const notes = await loadNotes(user.uid);
+                dispatch(setNotes(notes));
+
             } else { 
                 setIsLoggedIn(false);
             }
