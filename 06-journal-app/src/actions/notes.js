c7d60d58c3d1fd66a5806ff8305f255b94055e55
obbijuan <juan.garces@outlook.com>
2021-01-30 23:51:32 -0300

    Upload image to Cloudinary

--- 06-journal-app/src/actions/notes.js
+++ 06-journal-app/src/actions/notes.js
@@ -1,6 +1,7 @@
 import Swal from 'sweetalert2';
 
 import { db } from "../firebase/firebase-config";
+import { fileUpload } from '../helpers/fileUpload';
 import { loadNotes } from "../helpers/loadNotes";
 import { types } from "../types/types";
 
@@ -77,3 +78,14 @@ export const refreshNote = ( id, note ) => ({
         }
     }
 });
+
+export const startUploading = (file) => {
+    return async(dispatch, getState) => {
+
+        const { active: activeNote } = getState().notes;
+        
+        const fileUrl = await fileUpload(file);
+        console.log(fileUrl)
+
+    }
+}
--- 06-journal-app/src/components/notes/NotesAppBar.js
+++ 06-journal-app/src/components/notes/NotesAppBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
 
@@ -10,14 +10,33 @@ export const NotesAppBar = () => {
     const handleSave = () => {
         dispatch( startSaveNote(active))
     }
+    const handlePictureClick = () => {
+        document.querySelector('#fileSelector').click();
+    }
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            dispatch(startUploading(file))
+        }
+    }
 
 
     return (
         <div className="notes__appbar">
             <span>01 enero 2021</span>
+            <input
+                id="fileSelector"
+                type="file"
+                name="file"
+                style={{ display: 'none' }}
+                onChange={ handleFileChange }
+            />
 
             <div>
-                <button className="btn">
+                <button
+                    className="btn"
+                    onClick={ handlePictureClick}
+                >
                     Picture
                 </button>
 
