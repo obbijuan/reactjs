c895ea2e7fb62b1266009d00b5f4bc8cd24778c9
obbijuan <juan.garces@outlook.com>
2021-02-02 00:04:27 -0300

    Adding modal on calendar

---
+++ 07-calendar-app/src/components/calendar/CalendarModal.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+};
+Modal.setAppElement('#root');
+
+export const CalendarModal = () => {
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
+
+    return (
+        <Modal
+            isOpen={ isOpen }
+            onRequestClose={ closeModal }
+            style={ customStyles }
+            closeTimeoutMS={ 200 }
+            className="modal"
+            overlayClassName="modal-fondo"
+        >
+            <h1>Holaaa</h1>
+            <hr/>
+            <h1>Hola de nuevo</h1>
+        </Modal>
+    )
+}
