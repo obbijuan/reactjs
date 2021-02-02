91244644fe3e81df5269e55bb0dbe8f61cf2b699
obbijuan <juan.garces@outlook.com>
2021-02-01 22:42:03 -0300

    Additional settings are added to the calendar

--- 07-calendar-app/src/components/calendar/CalendarScreen.js
+++ 07-calendar-app/src/components/calendar/CalendarScreen.js
@@ -3,7 +3,11 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -15,6 +19,21 @@ const events = [{
 }]
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+    };
+
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -24,6 +43,8 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
             />
             
         </div>
