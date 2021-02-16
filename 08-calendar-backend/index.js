5c0940a2790b22266bb6b5bc8be6cc0ee1df9350
obbijuan <juan.garces@outlook.com>
2021-02-15 22:49:04 -0300

    Environment variables and public folder

--- 08-calendar-backend/index.js
+++ 08-calendar-backend/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+
+console.log(process.env)
 
 const app = express();
 
-// Rutas
-app.get('/', (req, res) => {
-    
-    res.json({
-        ok: true
-    })
-})
+// Directorio Publico
+app.use( express.static('public') )
 
-app.listen(4000, () => {
-    console.log('Servidor corriendo en puerto 4000!')
+
+// Rutas
+// app.get('/', (req, res) => {
+    
+//     res.json({
+//         ok: true
+//     })
+// })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}!`)
 })
