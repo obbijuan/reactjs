6f5433c1df1fdd1772652ef8941efeab9a38e218
obbijuan <juan.garces@outlook.com>
2021-02-16 00:32:35 -0300

    Endpoints of createUser, loginUser and renewToken

--- 08-calendar-backend/routes/auth.js
+++ 08-calendar-backend/routes/auth.js
@@ -7,12 +7,14 @@
 const { Router } = require('express');
 const router = Router();
 
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.get('/', (req, res) => {
-    
-    res.json({
-        ok: true
-    })
-})
+router.get('/new', createUser);
+
+router.post('/', loginUser);
+
+router.get('/renew', renewToken);
+
+
 
 module.exports = router;
