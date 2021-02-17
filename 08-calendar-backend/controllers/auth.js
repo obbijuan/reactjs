c5f4aa3c5dda39094745688415ee4705a5be4bf3
obbijuan <juan.garces@outlook.com>
2021-02-17 16:36:13 -0300

    User login

--- 08-calendar-backend/controllers/auth.js
+++ 08-calendar-backend/controllers/auth.js
@@ -41,9 +41,46 @@ const createUser = async(req, res = response) => {
 }
 
 
-const loginUser = (req, res = response) => {
+const loginUser = async(req, res = response) => {
 
     const { email, password } = req.body;
+
+    try {
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email no existe!'
+            });
+        }
+
+        // Confirmar las passwords
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password invalida!'
+            })
+        }
+        // Generar JWT
+
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        });
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor contacte al admin!'
+        })
+    }
     
     res.status(200).json({
         ok: true,
