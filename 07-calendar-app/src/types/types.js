769328f57362aad22a915b64e48e5efbb60aff0b
obbijuan <juan.garces@outlook.com>
2021-02-21 20:37:57 -0300

    AuthReducer added

--- 07-calendar-app/src/types/types.js
+++ 07-calendar-app/src/types/types.js
@@ -9,4 +9,12 @@ export const types = {
     eventUpdated: '[event] Event Updated',
     eventDeleted: '[event] Event Deleted',
 
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
 }
