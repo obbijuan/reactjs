ee4a3aa332d99c2e30a1ca0d0a75580cd293461f
obbijuan <juan.garces@outlook.com>
2021-03-25 00:43:16 -0300

    Test: Types must be equal

---
+++ 07-calendar-app/src/test/types/types.test.js
@@ -0,0 +1,33 @@
+import { types } from '../../types/types'
+
+
+describe('Pruebas en Types', () => {
+    test('los types deben de ser iguales', () => {
+        
+        expect(types).toEqual({
+
+            uiOpenModal: '[ui] Open modal',
+            uiCloseModal: '[ui] Close modal',
+        
+            eventSetActive: '[event] Set Active',
+            eventLogout: '[event] Logout event',
+        
+            eventStartAddNew: '[event] Start add new',
+            eventAddNew: '[event] Add new',
+            eventClearActiveEvent: '[event] Clear active event',
+            eventUpdated: '[event] Event Updated',
+            eventDeleted: '[event] Event Deleted',
+            eventLoaded: '[event] Events loaded',
+        
+            authCheckingFinish: '[auth] Finish checking login state',
+            authStartLogin: '[auth] Start login',
+            authLogin: '[auth] Login',
+            authStartRegister: '[auth] Start Register',
+            authStartTokenRenew: '[auth] Start token renew',
+            authLogout: '[auth] Logout',
+        
+        })
+
+    })
+    
+})
