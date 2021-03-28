b20e1307608ad16873e55186832b556d4b08e8c0
obbijuan <juan.garces@outlook.com>
2021-03-28 13:13:06 -0300

    Test on <CalendarModal />. Part 3

--- 07-calendar-app/src/test/components/calendar/CalendarModal.test.js
+++ 07-calendar-app/src/test/components/calendar/CalendarModal.test.js
@@ -8,11 +8,18 @@ import moment from 'moment';
 
 import '@testing-library/jest-dom';
 import { CalendarModal } from '../../../components/calendar/CalendarModal';
-import { eventStartUpdate, eventClearActiveEvent } from '../../../actions/events';
+import { eventStartUpdate, eventClearActiveEvent, eventStartAddNew } from '../../../actions/events';
+import { act } from '@testing-library/react';
+import Swal from 'sweetalert2';
+
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
+}))
 
 jest.mock('../../../actions/events', () => ({
     eventStartUpdate: jest.fn(),
-    eventClearActiveEvent: jest.fn()
+    eventClearActiveEvent: jest.fn(),
+    eventStartAddNew: jest.fn()
 }))
 
 const middlewares = [ thunk ];
@@ -85,6 +92,79 @@ describe('Pruebas en <CalendarModal />', () => {
         // Se espera que el titulo esté vacio porque la prueba anterior limpia el formulario
         expect(wrapper.find('input[name="title"]').hasClass('is-invalid')).toBe(true);
         
+    });
+
+
+    test('debe de crear un nuevo evento', () => {
+
+        const initState = {
+            calendar: {
+                events: [],
+                activeEvent: null
+            },
+            auth: {
+                uid: '123',
+                name: 'obbijuan'
+            },
+            ui: {
+                modalOpen: true
+            }
+        };
+        
+        const store = mockStore( initState );
+        store.dispatch = jest.fn();
+                
+        const wrapper = mount(
+            <Provider store={ store } >
+                <CalendarModal />
+            </Provider>
+        );
+        
+        wrapper.find('input[name="title"]').simulate('change',{
+            target: {
+                name: 'title',
+                value: 'Cumpleaños Feña'
+            }
+        });
+        
+        wrapper.find('form').simulate('submit', {
+            preventDefault(){}
+        });
+
+        expect( eventStartAddNew ).toHaveBeenCalledWith({
+            end: expect.anything(),
+            start: expect.anything(),
+            title:'Cumpleaños Feña',
+            notes: ''
+        });
+
+        expect( eventClearActiveEvent ).toHaveBeenCalled();
+
+    });
+
+
+
+    test('debe de validar las fechas', () => {
+
+        wrapper.find('input[name="title"]').simulate('change',{
+            target: {
+                name: 'title',
+                value: 'Cumpleaños Feña'
+            }
+        });
+
+        const hoy = new Date();
+
+        act(() => {
+            wrapper.find('DateTimePicker').at(1).prop('onChange')(hoy);
+        })
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault(){}
+        });
+
+        expect( Swal.fire ).toHaveBeenCalledWith("Error", "La fecha fin debe de ser mayor a la fecha de inicio", "error");
+
     });
 
 })
