9cb1571d74b699a8a1897ded66de33b387d8647b
obbijuan <juan.garces@outlook.com>
2021-03-28 00:34:40 -0300

    Test on <LoginScreen />. Part 2

--- 07-calendar-app/src/test/components/auth/LoginScreen.test.js
+++ 07-calendar-app/src/test/components/auth/LoginScreen.test.js
@@ -4,17 +4,22 @@ import { Provider } from 'react-redux';
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
 
 import '@testing-library/jest-dom';
 
 import { LoginScreen } from '../../../components/auth/LoginScreen';
-import { startLogin } from '../../../actions/auth';
+import { startLogin, startRegister } from '../../../actions/auth';
 
 
 jest.mock('../../../actions/auth', () => ({
-    startLogin: jest.fn()
+    startLogin: jest.fn(),
+    startRegister: jest.fn()
 }));
 
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
+}));
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -32,6 +37,10 @@ const wrapper = mount(
 
 
 describe('Pruebas en <LoginScreen />', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
 
 
     test('debe mostrarse correctamente', () => {
@@ -62,6 +71,60 @@ describe('Pruebas en <LoginScreen />', () => {
 
         expect( startLogin ).toHaveBeenCalledWith('obbijuan@gmail.com','123456');
 
+    });
+
+
+    test('No hay registro si las contraseñas son diferentes', () => {
+
+        wrapper.find('input[name="rPassword1"]').simulate('change', {
+            target: {
+                name: 'rPassword1',
+                value: '123456',
+            }
+        });
+
+        wrapper.find('input[name="rPassword2"]').simulate('change', {
+            target: {
+                name: 'rPassword2',
+                value: '1234567',
+            }
+        });
+        
+
+        wrapper.find('form').at(1).prop('onSubmit')({
+            preventDefault(){}
+        });
+
+        expect( startRegister ).not.toHaveBeenCalled();
+        expect( Swal.fire ).toHaveBeenCalledWith('Error', 'Las contraseñas no coinciden!', 'error');
+    
+    });
+
+
+    test('Registro con contraseñas iguales', () => {
+        
+        wrapper.find('input[name="rPassword1"]').simulate('change', {
+            target: {
+                name: 'rPassword1',
+                value: '123456789',
+            }
+        });
+
+        wrapper.find('input[name="rPassword2"]').simulate('change', {
+            target: {
+                name: 'rPassword2',
+                value: '123456789',
+            }
+        });
+        
+
+        wrapper.find('form').at(1).prop('onSubmit')({
+            preventDefault(){}
+        });
+
+        expect( Swal.fire ).not.toHaveBeenCalled();
+        expect( startRegister ).toHaveBeenCalledWith("obbijuan@gmail.com", "123456789", "obbijuan");
+    
     });
 
 });
